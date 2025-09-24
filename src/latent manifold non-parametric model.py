63d2465d1d1f5cc7339285839519a259fb4ac9a6
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2025-09-24 00:27:16 -0400

    Pushig the data and the file to the repo

---
+++ src/latent manifold non-parametric model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+data = pd.read_csv('sub_sample.csv')
+
+# Explore the data
+print(data.head())
